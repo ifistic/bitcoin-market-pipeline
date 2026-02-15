3cf28951fbcbf90c284ac12a0b4d26233046cc95
IFIOK UDOH <125509646+ifistic@users.noreply.github.com>
2026-02-15 17:02:57 +0000

    Update dbt job definitions to use v2 API
    Refactor dbt job definitions to use v2 API and remove old implementations.

--- crypto_pipeline/jobs.py
+++ crypto_pipeline/jobs.py
@@ -1,24 +1,24 @@
 # crypto_pipeline/jobs.py
 from dagster import job
 from crypto_pipeline.assets import coingecko_crypto_market
-from dagster import job, op
+
 from dagster_dbt import dbt_run_op
 
 from .resources import dbt_resource
+from dagster import job
+from crypto_pipeline.assets import coingecko_crypto_market
+from .resources import dbt_resource
+from dagster_dbt import dbt_run_op_v2, dbt_test_op_v2  # <-- v2 API
 
+@job(resource_defs={"dbt": dbt_resource})
+def run_dbt_all():
+    dbt_run_op_v2(models=["all"], resource_key="dbt")
+
+@job(resource_defs={"dbt": dbt_resource})
+def run_dbt_gold():
+    dbt_run_op_v2(models=["Gold"], resource_key="dbt")
 @job
 def crypto_market_job():
     coingecko_crypto_market()
 
-# Option 1: Run all models
-@job
-def run_dbt_all():
-    dbt_run_op()
 
-
-# ----------------------------
-# Run ONLY Gold models
-# ----------------------------
-@job
-def run_dbt_gold():
-    dbt_run_op.configured({"select": "Gold"})()
