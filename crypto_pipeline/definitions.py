422e761f29f4cb3f90c4d00edadacc9269809126
IFIOK UDOH <125509646+ifistic@users.noreply.github.com>
2026-02-15 08:53:11 +0000

    Integrate dbt CLI resource into definitions
    Added dbt CLI resource configuration for DBT integration.

--- crypto_pipeline/definitions.py
+++ crypto_pipeline/definitions.py
@@ -3,10 +3,18 @@ from dagster import Definitions
 from .assets import coingecko_crypto_market
 from .jobs import crypto_market_job
 from .schedules import daily_crypto_market_schedule
+from dagster_dbt import dbt_cli_resource
+
 
 defs = Definitions(
     assets=[coingecko_crypto_market],
     jobs=[crypto_market_job],
     schedules=[daily_crypto_market_schedule],
+)
+dbt_resource = dbt_cli_resource.configured(
+    {
+        "project_dir": "./bitcoin_dbt",  # path to my dbt project
+        "profiles_dir": "~/.dbt",       # path to my profiles.yml
+    }
 )
 
