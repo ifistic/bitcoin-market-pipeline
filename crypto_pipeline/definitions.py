760eaa1e6625c568f8386bac96dd6a9e868c6bed
IFIOK UDOH <125509646+ifistic@users.noreply.github.com>
2026-02-15 15:44:01 +0000

    Configure dbt CLI resource in definitions.py

--- crypto_pipeline/definitions.py
+++ crypto_pipeline/definitions.py
@@ -5,16 +5,20 @@ from .jobs import crypto_market_job
 from .schedules import daily_crypto_market_schedule
 from dagster_dbt import dbt_cli_resource_v2
 
-
+# Configure dbt CLI resource (v2)
+dbt_resource = dbt_cli_resource_v2.configured(
+    {
+        "project_dir": "/home/ifi/bitcoin_market/bitcoin_dbt",
+        "profiles_dir": "/home/ifi/.dbt",
+    }
+)
 
 defs = Definitions(
     assets=[coingecko_crypto_market],
     jobs=[crypto_market_job],
     schedules=[daily_crypto_market_schedule],
+    resources={
+        "dbt": dbt_resource,
+    },
 )
-dbt = dbt_cli_resource_v2.configured({
-    "project_dir": "/home/ifi/bitcoin_market/bitcoin_dbt",
-    "profiles_dir": "/home/ifi/.dbt"
-})
-
 
