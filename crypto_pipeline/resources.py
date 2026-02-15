b4b7efc91c2a740f8ca05a2010c733472f93e52b
ifistic <ifistic@gmail.com>
2026-02-15 17:47:50 +0000

    Add Dagster DBT resource

---
+++ crypto_pipeline/resources.py
@@ -0,0 +1,9 @@
+# crypto_pipeline/resources.py
+from dagster_dbt import dbt_cli_resource_v2
+
+dbt_resource = dbt_cli_resource_v2.configured(
+    {
+        "project_dir": "/home/ifi/bitcoin_market/bitcoin_dbt",
+        "profiles_dir": "/home/ifi/.dbt",
+    }
+)
