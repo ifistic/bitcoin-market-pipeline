af2d79d68ab7856278c9df97aa5cee16c6ff1824
IFIOK UDOH <125509646+ifistic@users.noreply.github.com>
2026-02-16 07:27:02 +0000

    Update definitions.py

--- crypto_pipeline/definitions.py
+++ crypto_pipeline/definitions.py
@@ -1,29 +1,12 @@
 # crypto_pipeline/definitions.py
 from dagster import Definitions
+from dagster_dbt import dbt_cli_resource_v2
+
 from .assets import coingecko_crypto_market
+from .dbt_assets import *  # include all dbt asset defs if needed
 from .jobs import crypto_market_job
 from .schedules import daily_crypto_market_schedule
-from dagster_dbt import dbt_cli_resource_v2
 from .resources import dbt_resource
-
-from dagster import Definitions
-
-from .assets import *
-from .dbt_assets import *
-from .jobs import crypto_market_job
-from .resources import dbt_resource
-from .schedules import crypto_market_schedule
-
-
-defs = Definitions(
-    assets=[*globals().values()],
-    jobs=[crypto_market_job],
-    schedules=[crypto_market_schedule],
-    resources={
-        "dbt": dbt_resource,
-    },
-)
-
 
 # Configure dbt CLI resource (v2)
 dbt_resource = dbt_cli_resource_v2.configured(
@@ -33,27 +16,12 @@ dbt_resource = dbt_cli_resource_v2.configured(
     }
 )
 
-
+# Single Definitions object
 defs = Definitions(
-    assets=[coingecko_crypto_market],
+    assets=[coingecko_crypto_market],  # add more assets if needed
     jobs=[crypto_market_job],
     schedules=[daily_crypto_market_schedule],
     resources={
         "dbt": dbt_resource,
     },
 )
-# crypto_pipeline/definitions.py
-from dagster import Definitions
-from .assets import coingecko_crypto_market
-from .jobs import crypto_market_job
-from .schedules import daily_crypto_market_schedule
-
-defs = Definitions(
-    assets=[coingecko_crypto_market],
-    jobs=[crypto_market_job],
-    schedules=[daily_crypto_market_schedule],
-    resources={
-        "dbt": dbt_resource,
-    },
-)
-
