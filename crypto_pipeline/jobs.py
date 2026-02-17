c87a6cdff6921575bcde7ec0d5c137bb19694b49
IFIOK UDOH <125509646+ifistic@users.noreply.github.com>
2026-02-17 00:46:38 +0000

    Update jobs.py

--- crypto_pipeline/jobs.py
+++ crypto_pipeline/jobs.py
@@ -1,21 +1,8 @@
 # crypto_pipeline/jobs.py
 from dagster import job
-from crypto_pipeline.assets import coingecko_crypto_market
-from .resources import dbt_resource
-from dagster import job
-from crypto_pipeline.assets import coingecko_crypto_market
-from .resources import dbt_resource
-from dagster_dbt import dbt_run_op_v2, dbt_test_op_v2  # <-- v2 API
+from .assets import coingecko_crypto_market
 
-@job(resource_defs={"dbt": dbt_resource})
-def run_dbt_all():
-    dbt_run_op_v2(models=["all"], resource_key="dbt")
-
-@job(resource_defs={"dbt": dbt_resource})
-def run_dbt_gold():
-    dbt_run_op_v2(models=["Gold"], resource_key="dbt")
 @job
 def crypto_market_job():
+    """Job to fetch crypto market data from CoinGecko"""
     coingecko_crypto_market()
-
-
